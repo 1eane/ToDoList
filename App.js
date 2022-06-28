d6e4e576e0fe4833c8f1a716a73e4eaad2b543fb
Léane <drsleane@gmail.com>
2022-06-28 12:25:50 +0200

    update js

--- App.js
+++ App.js
@@ -37,3 +37,22 @@ function addTodo(event) {
   todoList.appendChild(todoDiv);
   todoInput.value = "";
 }
+
+function deleteCheck(e) {
+  const item = e.target;
+  //DELETE
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    todo.classList.add("fall");
+    removeLocalTodos(todo);
+    todo.addEventListener("transitionend", function () {
+      todo.remove();
+    });
+  }
+
+  //CHECK MARK
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
+}
