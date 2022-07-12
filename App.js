03cd58162f0d6aa7ef4a1e55540070916f1e1d3a
Léane <drsleane@gmail.com>
2022-07-12 16:52:04 +0200

    update only App.js (comments)

--- App.js
+++ App.js
@@ -13,28 +13,26 @@ filterOption.addEventListener("input", filterTodo);
 
 //FUNCTIONS
 function addTodo(event) {
-// function addTodo(event) {
   event.preventDefault();
-  // event.preventDefault();
   const todoDiv = document.createElement("div");
-  // const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
-  //todoDiv.classList.add
 
+// const newTodo
 
   const newTodo = document.createElement("li");
-  // const newTodo = document.createElement("li");
   newTodo.innerText = todoInput.value;
-  // const newTodo = document.createElement("li");
   newTodo.classList.add("todo-item");
   todoDiv.appendChild(newTodo);
   saveLocalTodos(todoInput.value);
 
+// const completedButton
 
   const completedButton = document.createElement("button");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
   completedButton.classList.add("complete-btn");
   todoDiv.appendChild(completedButton);
+
+  // const trashButton
 
   const trashButton = document.createElement("button");
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
